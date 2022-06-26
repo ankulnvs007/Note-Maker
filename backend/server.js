1ef8b8c29647aba8883163fc799aef2691901bb5
Ankul Rathour <ankulnvs007@gmail.com>
2022-06-26 14:15:54 +0530

    Note Maker First Commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import path from "path";
+
+import noteRoutes from "./routes/noteRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express(); // main thing
+
+app.use(express.json()); // to accept json data
+
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
+app.use("/api/notes", noteRoutes);
+app.use("/api/users", userRoutes);
+
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running on port ${PORT}`.yellow
+      .bold
+  )
+);
